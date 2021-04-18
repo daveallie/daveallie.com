c80c041d2b976d7563fed5b1c2b4f95130e8abae
Dave Allie <dave@daveallie.com>
2021-04-18 15:55:52 +1000

    Add Figure grids

--- src/components/Figure/index.tsx
+++ src/components/Figure/index.tsx
@@ -1,14 +1,17 @@
 import React, { Children, ReactElement, ReactNode } from 'react';
+import cn from 'classnames';
+import { chunk } from 'lodash';
 import * as styles from './styles.module.scss';
 
 type FigureProps = {
-  children: ReactNode;
+  grid?: boolean;
   caption?: string;
+  children: ReactNode;
 };
 
 const removeWrappingP = (children: ReactNode) => {
   if (Children.count(children) !== 1) {
-    return children;
+    return Children.toArray(children);
   }
 
   const onlyChild = Children.only(children)!! as ReactElement;
@@ -17,17 +20,31 @@ const removeWrappingP = (children: ReactNode) => {
     onlyChild.type.displayName === 'MDXCreateElement' &&
     onlyChild.props.originalType === 'p'
   ) {
-    return <>{onlyChild.props.children}</>;
+    return Children.toArray(onlyChild.props.children).filter((c) => c !== '\n');
   }
 
-  return children;
+  return Children.toArray(children);
 };
 
 export default function Figure(props: FigureProps) {
+  const children = removeWrappingP(props.children);
+  const childrenChunks = chunk(children, props.grid ? 2 : 1);
+
   return (
     <figure>
-      <div className={styles.figureGrid}>{removeWrappingP(props.children)}</div>
-      {props.caption && <figcaption>{props.caption}</figcaption>}
+      <div
+        className={cn(
+          styles.container,
+          props.grid ? styles.grid : styles.notGrid
+        )}
+      >
+        {childrenChunks.map((cc) => (
+          <div className={styles.row}>{cc}</div>
+        ))}
+      </div>
+      {props.caption && (
+        <figcaption className={styles.caption}>{props.caption}</figcaption>
+      )}
     </figure>
   );
 }
