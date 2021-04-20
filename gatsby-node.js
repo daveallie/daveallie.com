a8692bac5adf7447a626d961c58defdfd7c0c3c0
Dave Allie <dave@daveallie.com>
2021-04-20 22:08:20 +1000

    Disable sourcemaps in prod

--- gatsby-node.js
+++ gatsby-node.js
@@ -91,3 +91,11 @@ exports.onCreatePage = ({ page, actions }) => {
 
   createPage(newPage);
 };
+
+exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
+  if (getConfig().mode === 'production') {
+    actions.setWebpackConfig({
+      devtool: false,
+    });
+  }
+};
