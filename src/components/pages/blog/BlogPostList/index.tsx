81fadfbf12e341ce43f4f3be341d75e1969b6fb3
Dave Allie <dave@daveallie.com>
2021-04-28 20:48:43 +1000

    Add additional padding to blog post index

--- src/components/pages/blog/BlogPostList/index.tsx
+++ src/components/pages/blog/BlogPostList/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import cn from 'classnames';
 import useBlogPostListQuery, {
   BlogPostListQueryNode,
 } from '../../../../hooks/queries/useBlogPostListQuery';
@@ -23,11 +24,22 @@ export default function BlogPostList() {
 
   return (
     <Text container="div" className={styles.container}>
-      {keys.map((year) => (
-        <ContentBlock negTopMargin={year === keys[0]} key={year}>
-          <Text weight={800}>{year}</Text>
-          {yearData[year].map((node) => (
-            <div key={node.id} className={styles.postRow}>
+      {keys.map((year, yearI) => (
+        <ContentBlock negTopMargin={yearI === 0} key={year}>
+          <Text
+            container="div"
+            weight={800}
+            className={cn({ [styles.firstYearRow]: yearI === 0 })}
+          >
+            {year}
+          </Text>
+          {yearData[year].map((node, postI) => (
+            <div
+              key={node.id}
+              className={cn(styles.postRow, {
+                [styles.firstPostRow]: postI === 0,
+              })}
+            >
               <div className={styles.postDate}>{node.frontmatter.date}</div>
               <div>
                 <Link href={`/${node.frontmatter.slug}`} gatsby>
