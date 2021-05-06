e398c35bef63348234041a0e721a12687964d13b
Dave Allie <dave@daveallie.com>
2021-05-06 19:55:04 +1000

    Add RSS link to blog pages

---
+++ src/components/Icon/RSS.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import cn from 'classnames';
+import * as styles from './styles.module.scss';
+
+export default function RSS({
+  accentColor = 'light',
+}: {
+  accentColor?: 'dark' | 'light';
+}) {
+  const accentStyles = cn(styles.noStroke, {
+    [styles.forceDarkFill]: accentColor === 'dark',
+    [styles.forceLightFill]: accentColor === 'light',
+  });
+
+  return (
+    <div className={styles.container}>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 8 8">
+        <title>RSS feed icon</title>
+
+        <rect className={styles.noStroke} width="8" height="8" rx="1.5" />
+        <circle className={accentStyles} cx="2" cy="6" r="1" />
+        <path
+          className={accentStyles}
+          d="m 1,4 a 3,3 0 0 1 3,3 h 1 a 4,4 0 0 0 -4,-4 z"
+        />
+        <path
+          className={accentStyles}
+          d="m 1,2 a 5,5 0 0 1 5,5 h 1 a 6,6 0 0 0 -6,-6 z"
+        />
+      </svg>
+    </div>
+  );
+}
--- src/components/PageFooter/index.tsx
+++ src/components/PageFooter/index.tsx
@@ -2,17 +2,22 @@ import React from 'react';
 import cn from 'classnames';
 import Github from '../Icon/Github';
 import LinkedIn from '../Icon/LinkedIn';
+import RSS from '../Icon/RSS';
 import Link from '../Link';
 import Text from '../Text';
 import * as styles from './styles.module.scss';
 
+type PageFooterProps = {
+  includeHomeLink?: boolean;
+  fixed?: boolean;
+};
+
 export default function PageFooter({
   includeHomeLink = true,
   fixed = false,
-}: {
-  includeHomeLink?: boolean;
-  fixed?: boolean;
-}) {
+}: PageFooterProps) {
+  const includeRssLink = process.env.SUBSITE === 'blog';
+
   return (
     <div className={cn(styles.footer, { [styles.fixed]: fixed })}>
       <div className={styles.footerItem}>
@@ -24,6 +29,13 @@ export default function PageFooter({
       </div>
 
       <div className={styles.socialLinks}>
+        {includeRssLink && (
+          <div className={styles.footerItem}>
+            <Link color="ultraLight" href="/rss.xml">
+              <RSS accentColor="dark" />
+            </Link>
+          </div>
+        )}
         <div className={styles.footerItem}>
           <Link
             color="ultraLight"
