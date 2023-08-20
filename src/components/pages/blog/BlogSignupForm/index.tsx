fc0c21b3bf7033f8bae8402c52e59d13d888938b
Dave Allie <dave@daveallie.com>
2023-08-20 12:03:07 +1000

    Migrate mailerlite

--- src/components/pages/blog/BlogSignupForm/index.tsx
+++ src/components/pages/blog/BlogSignupForm/index.tsx
@@ -4,10 +4,6 @@ import * as styles from './styles.module.scss';
 
 export default function BlogSignupForm() {
   return (
-    <div
-      className={cn('ml-form-embed', styles.container)}
-      data-account="3822203:t5f7k3w2h6"
-      data-form="5471996:t0i1v9"
-    />
+    <div className={cn('ml-embedded', styles.container)} data-form="WHFMJZ" />
   );
 }
